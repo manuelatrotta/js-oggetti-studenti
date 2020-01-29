9d10e8bdda988063aa92c63dcb790b3232b3c456
manuelatrotta <manuelat88@hotmail.it>
2020-01-29 18:51:17 +0100

    creazione messaggio automatico

--- handlebars/js/script.js
+++ handlebars/js/script.js
@@ -1,20 +1,46 @@
+//al click sul bottone deve partire il mesaggio inserito con la data
 $(document).ready(function() {
   $('.send').click(function() {
+  //variabile del nuovo testo con val() inserita in seguito dall'utente
     var textNew = $('.send-msg').val();
-
+//variabili per time
     var data = new Date();
     var hours = data.getHours();
     var minutes = data.getMinutes();
     var time = hours + ':' + minutes;
-
+//variabili in handlebarsj correlate a script entry-template
     var source = $('#entry-template').html();
     var template = Handlebars.compile(source);
-
+//var handlebarsj in cui decidiamo di introdurre variabili create prima
     var context = {
       'message' : textNew,
-      'date' : time
+      'date' : time,
+      'sentReceived' : 'sent'
     };
+//aggiunta template in #add su html
     var html = template(context);
-    $('#add').append(html)
+    $('#add').append(html);
+
+//messaggio di risposta automatico con setTimeout
+setTimeout(function() {
+  var response = 'yes, of course';
+
+  var data = new Date();
+  var hours = data.getHours();
+  var minutes = data.getMinutes();
+  var time = hours + ':' + minutes;
+
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+
+  var context = {
+    'message' : response,
+    'date' : time,
+    'sentReceived' : 'received'
+  };
+    var html = template(context);
+
+    $('#add').append(html);
+    }, 2000);
   })
 });
