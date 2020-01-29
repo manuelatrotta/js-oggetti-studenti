07d15f2e6430e65872f6b04bbcde8f1d17a25ec1
manuelatrotta <manuelat88@hotmail.it>
2020-01-29 15:20:37 +0100

    creazione oggetto e stampa caratteristiche con for in

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Esercizio 1
+//Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà
+var studente = {
+  'nome': 'Santino',
+  'cognome': 'Vito',
+  'eta' : '28',
+};
+
+for (var key in studente) {
+  console.log(studente[key]);
+}
+//Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+//Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
